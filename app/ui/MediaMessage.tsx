0f48c6a50eb206f79e4c4ecab891251e6e7c7250
Dev-Dylann <tubitunmise@gmail.com>
2024-06-20 22:52:48 +0100

    added fullscreen view to media messages

--- app/ui/MediaMessage.tsx
+++ app/ui/MediaMessage.tsx
@@ -50,7 +50,21 @@ export default function MediaMessage({ message, replied, setReplying }: Props) {
     const repliedMedia = replied?.media as any
 
     return (
-        <div id={`${message.id}`} /* onDoubleClick={() => replyActive(message.id)} */ className='border rounded-lg p-2 my-1 flex flex-col gap-1 w-fit max-w-[80vw] scroll-mt-40 backdrop-blur message'>
+        <div id={`${message.id}`} /* onDoubleClick={() => replyActive(message.id)} */ className={!fullscreen ? `border rounded-lg p-2 my-1 flex flex-col gap-1 w-fit max-w-[75vw] scroll-mt-40 backdrop-blur message transition-all` : `fixed top-0 left-0 w-full h-full z-[11] backdrop-blur transition-all scroll-mt-40 flex flex-col gap-4 px-5 py-8`}>
+
+            {fullscreen && (
+                <>
+                    <button type="button" onClick={() => setFullscreen(false)} className='p-2 rounded-lg border w-fit'>
+                        <XMarkIcon className='h-5 w-5' />
+                    </button>
+
+                    <a href={process.env.NEXT_PUBLIC_SUPABASE_BUCKET_URL! + media.path} className='bg-white text-darkBg rounded-lg self-center py-2 px-5 w-fit flex gap-2 font-semibold order-1'>
+                        <ArrowDownTrayIcon className='h-5 w-5' />
+                        Download
+                    </a>
+                </>
+            )}
+
             {replied && !replied.is_media && (
                 <Link href={`#${replied.id}`}>
                     <pre className={`${inter.className} p-2 border rounded line-clamp-3 text-xs text-wrap text-ellipsis`}>{replied.message}</pre>
@@ -64,7 +78,7 @@ export default function MediaMessage({ message, replied, setReplying }: Props) {
                 </Link>
             )}
 
-            <div className='flex items-center gap-2 text-sm overflow-hidden max-h-[40vh]' onClick={() => setFullscreen(true)}>
+            <div className={!fullscreen ? 'flex items-center gap-2 text-sm overflow-hidden max-h-[40vh] rounded' : 'grow flex justify-center items-center overflow-hidden'} onClick={() => setFullscreen(true)}>
                 {media.type.startsWith('image') ? (
                     <Image
                         src={process.env.NEXT_PUBLIC_SUPABASE_BUCKET_URL! + media.path}
@@ -72,18 +86,18 @@ export default function MediaMessage({ message, replied, setReplying }: Props) {
                         width={media.width}
                         height={media.height}
                         quality={50}
-                        className='rounded'
+                        className={fullscreen ? 'max-h-full w-fit max-w-full' : ''}
                     ></Image>
                 ) : (
-                    <video controls className='rounded'>
+                    <video controls>
                         <source src={process.env.NEXT_PUBLIC_SUPABASE_BUCKET_URL! + media.path} type={media.type} />
                     </video>
                 )}
             </div>
 
-            <span className='text-[10px] text-gray-400'>{datetime}</span>
+            <span className={!fullscreen ? 'text-[10px] text-gray-400' : 'hidden'}>{datetime}</span>
 
-            {fullscreen && (
+            {/* {fullscreen && (
                 <div className='fixed top-0 left-0 h-full w-full backdrop-blur px-5 py-8 flex flex-col gap-4'>
                     <button type="button" onClick={() => setFullscreen(false)} className='p-2 rounded-lg border w-fit'>
                         <XMarkIcon className='h-5 w-5' />
@@ -115,7 +129,7 @@ export default function MediaMessage({ message, replied, setReplying }: Props) {
                 </div>
 
 
-            )}
+            )} */}
         </div>
     )
 }
