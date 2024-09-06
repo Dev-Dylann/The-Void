22227a0cee8ee5fe8daa73299f5ac8e932595991
kunmi Tubi <tubitunmise@gmail.com>
2024-09-06 23:11:15 +0100

    touched gradient in index for md screen

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <main className='relative row-span-full flex flex-col'>
 
-      <section className='w-full h-full bg-gradient-to-b from-transparent from-10% to-darkBg to-80% p-5 pb-12 flex flex-col justify-end text-center sm:p-10 sm:pb-16'>
+      <section className='w-full h-full bg-gradient-to-b from-transparent from-10% to-darkBg to-80% p-5 pb-12 flex flex-col justify-end text-center sm:p-10 sm:pb-16 md:pb-24 md:from-5% md:to-70%'>
         <p className={`${montserrat.className} font-semibold text-lg sm:text-2xl`}>Welcome to</p>
         <h1 className={`${astro.className} font-semibold text-[65px] -mt-2 sm:text-[80px]`}>The Void</h1>
 
